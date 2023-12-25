fb25ab2a41a8ed9654246c3a736cdbeb3d90169e
AndyC <ychen276@students.cs.ubc.ca>
2023-12-24 21:14:57 -0800

    Started

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,18 +1,42 @@
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
+import io.github.cdimascio.dotenv.Dotenv;
+import listeners.EventListener;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
 import javax.security.auth.login.LoginException;
 
 public class DiscordBot {
 
-    public static void main(String[] args) throws LoginException {
-        final String token = "MTE4ODY3NzgxNTUxMjYxMjkxNQ.GO1LaQ.d7K0KTQBvDO8_z2QNnwXQ6wKi1Bunqf1kGklzk";
+    private final Dotenv config;
+    private final ShardManager shardManager;
 
-        JDA bot = JDABuilder.createDefault(token)
-                .setActivity(Activity.playing("a"))
-                .setStatus(OnlineStatus.OFFLINE)
-                .build();
+    public DiscordBot() throws LoginException {
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.playing("Testing"));
+        shardManager = builder.build();
+
+        shardManager.addEventListener(new EventListener());
+    }
+
+    public Dotenv getConfig() {
+        return config;
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public static void main(String[] args) {
+        try {
+            DiscordBot bot = new DiscordBot();
+        } catch (LoginException e) {
+            System.out.println("Token is invalid");
+        }
     }
 }
